dbcfacb24d809e75f9f48854fb69b772b010c73d
xocye <xocye20@hotmail.com>
2017-04-30 17:19:51 -0600

    Se agrego  tablas  para la interfaz grafica, esto  para las funcionalidades

--- app/src/main/java/com/example/xocye/dopingdetector/MainActivity.java
+++ app/src/main/java/com/example/xocye/dopingdetector/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-// Doping Detector :-)
+// Doping Detector
 public class MainActivity extends AppCompatActivity {
 
     /**
