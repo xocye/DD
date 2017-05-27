928aeabcdc9816e7bd0567deb721dfab245e07c7
chac0n <chacotri@hotmail.com>
2017-05-26 19:04:37 -0600

    Escaner agregado.

--- app/src/main/java/com/example/xocye/dopingdetector/fragment/Tab2Scan.java
+++ app/src/main/java/com/example/xocye/dopingdetector/fragment/Tab2Scan.java
@@ -1,21 +1,140 @@
 package com.example.xocye.dopingdetector.fragment;
 
-
-import android.support.v4.app.Fragment;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
+import com.example.xocye.dopingdetector.CameraPreview;
 import com.example.xocye.dopingdetector.R;
 
+import net.sourceforge.zbar.Config;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
 
-public class Tab2Scan extends Fragment{
+public class Tab2Scan extends Fragment {
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Handler autoFocusHandler;
+    private ImageScanner scanner;
 
+    private boolean barcodeScanned = false;
+    private boolean previewing = true;
+
+    static {
+        System.loadLibrary("iconv");
+    }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab2scan, container, false);
+
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        autoFocusHandler = new Handler();
+        mCamera = getCameraInstance();
+
+        // Instance barcode scanner
+        scanner = new ImageScanner();
+        scanner.setConfig(0, Config.X_DENSITY, 3);
+        scanner.setConfig(0, Config.Y_DENSITY, 3);
+
+        mPreview = new CameraPreview(rootView.getContext(), mCamera, previewCb,
+                autoFocusCB);
+        FrameLayout preview = (FrameLayout) rootView.findViewById(R.id.cameraPreview);
+        preview.addView(mPreview);
+
         return rootView;
     }
+
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e) {
+        }
+        return c;
+    }
+
+    private Runnable doAutoFocus = new Runnable() {
+        public void run() {
+            if (previewing)
+                mCamera.autoFocus(autoFocusCB);
+        }
+    };
+
+    Camera.PreviewCallback previewCb = new Camera.PreviewCallback() {
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            Camera.Parameters parameters = camera.getParameters();
+            Camera.Size size = parameters.getPreviewSize();
+
+            Image barcode = new Image(size.width, size.height, "Y800");
+            barcode.setData(data);
+
+            int result = scanner.scanImage(barcode);
+
+            if (result != 0) {
+                previewing = false;
+                mCamera.setPreviewCallback(null);
+                mCamera.stopPreview();
+
+                SymbolSet syms = scanner.getResults();
+                for (Symbol sym : syms) {
+
+                    Log.i("<<<<<<Asset Code>>>>> ",
+                            "<<<<Bar Code>>> " + sym.getData());
+                    String scanResult = sym.getData().trim();
+                    barcodeScanned = true;
+                    showAlertDialog(scanResult);
+
+                  /*  Toast.makeText(BarcodeScanner.this, scanResult,
+                            Toast.LENGTH_SHORT).show();*/
+
+
+
+                    break;
+                }
+            }
+        }
+    };
+
+    // Mimic continuous auto-focusing
+    Camera.AutoFocusCallback autoFocusCB = new Camera.AutoFocusCallback() {
+        public void onAutoFocus(boolean success, Camera camera) {
+            autoFocusHandler.postDelayed(doAutoFocus, 1000);
+        }
+    };
+
+
+    private void showAlertDialog(String message) {
+
+        new AlertDialog.Builder(getContext())
+                .setTitle(getResources().getString(R.string.app_name))
+                .setCancelable(false)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (barcodeScanned) {
+                            barcodeScanned = false;
+                            mCamera.setPreviewCallback(previewCb);
+                            mCamera.startPreview();
+                            previewing = true;
+                            mCamera.autoFocus(autoFocusCB);
+                        }
+                    }
+                }).show();
+    }
+
 }
